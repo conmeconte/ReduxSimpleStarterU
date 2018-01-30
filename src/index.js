4724b7ee89788e6f948722408288a2325a451f38
conmeconte <32534202+conmeconte@users.noreply.github.com>
2018-01-29 23:25:44 -0800

    finished step1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+// import App from './components/app';
 import reducers from './reducers';
+import SearchBar from './components/search_bar';
+
+const API_KEY= 'AIzaSyAz-cr7tW2g8-6gwVViqcuuPt1Y1nO5v6c';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+const App = ()=>{
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
