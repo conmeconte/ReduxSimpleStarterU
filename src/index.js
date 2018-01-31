1633fa3d801e9180605433658789976e9690e145
conmeconte <32534202+conmeconte@users.noreply.github.com>
2018-01-30 23:56:34 -0800

    working on step 2

--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+const API_KEY= 'AIzaSyAz-cr7tW2g8-6gwVViqcuuPt1Y1nO5v6c';
+import VideoList from './components/video_list'
 
 // import App from './components/app';
 import reducers from './reducers';
 import SearchBar from './components/search_bar';
 
-const API_KEY= 'AIzaSyAz-cr7tW2g8-6gwVViqcuuPt1Y1nO5v6c';
-
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 
-const App = ()=>{
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+      videos: []
+    }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos=>{
+      // this.setState({videos: videos}); 
+      this.setState({videos}); 
+    })
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+
 }
 
 ReactDOM.render(
